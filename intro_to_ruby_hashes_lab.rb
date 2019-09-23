6dcdbde81966e1b3d74028b0a2d14fac2cdb8513
Charity Parks <tuppercharity@gmail.com>
2019-09-23 02:07:11 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,8 @@ end
 
 
 def monopoly_with_third_tier
-	#copy and paste the monopoly hash you built in the previous method. We're going to add stuff to it here!
+
+#sets the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose value is the integer 25 (FAILED - 1)
 
 end
 
