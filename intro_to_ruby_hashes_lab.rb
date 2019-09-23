18070b7820f9464c4d3998a4130f3d89ceb5afc7
Charity Parks <tuppercharity@gmail.com>
2019-09-23 02:29:47 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,28 +26,8 @@ end
 
 
 def monopoly_with_third_tier
-  	monopoly = {
-		:railroads => {
-			:pieces => 4,
-			:names => {
-			  {:reading_railroad => {},
-			   :Pennsylvania__railroad => {},
-			   :b_and_o_railroad => {},
-			   :shortline => {}
-			},
-			:rent_in_dollars => {
-			  {:one_piece_owned => 25,
-			    :two_pieces_owned => 50,
-			    :three_pieces_owned => 100,
-			    :four_pieces_owned => 200
-			}
-		}
-	}
+  
 
-end
-#sets the 1st key of the :rent_in_dollars hash to a symbol, :one_piece_owned, whose value is the integer 25 (FAILED - 1)
-
-end
 
 def monopoly_with_fourth_tier
 	#copy and past the monopoly hash you build in the previous method. We're going to add stuff to it here!
